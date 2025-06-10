93ad60517c70bb1eb29ad423ef1491df12ae6847
abdurrh <78967004+abdurrh@users.noreply.github.com>
2025-06-10 14:00:20 +0100

    Update and rename races.json to races.py

---
+++ races.py
@@ -0,0 +1,35 @@
+# All races and their stats & equipment slots in one big Python dictionary
+races = {
+    "Human": {
+        "stats": {"Strength": 4, "Agility": 4, "Defence": 4, "Luck": 4, "Life": 4, "Wisdom": 5, "Charisma": 5},
+        "equipment_slots": ["helmet", "body_armor", "leg_armor", "boots", "left_ring", "right_ring", "left_bracelet", "right_bracelet", "left_hand", "right_hand", "amulet", "belt", "cloak", "gloves", "back"]
+    },
+    "Elf": {
+        "stats": {"Strength": 2, "Agility": 6, "Defence": 2, "Luck": 3, "Life": 3, "Wisdom": 8, "Charisma": 6},
+        "equipment_slots": ["helmet", "body_armor", "leg_armor", "boots", "left_ring", "right_ring", "left_bracelet", "right_bracelet", "left_hand", "right_hand", "amulet", "belt", "cloak", "gloves", "back"]
+    },
+    "Dwarf": {
+        "stats": {"Strength": 4, "Agility": 2, "Defence": 7, "Luck": 2, "Life": 7, "Wisdom": 4, "Charisma": 4},
+        "equipment_slots": ["helmet", "body_armor", "leg_armor", "boots", "left_ring", "right_ring", "left_bracelet", "right_bracelet", "left_hand", "right_hand", "amulet", "belt", "cloak", "gloves", "back"]
+    },
+    "Halfling": {
+        "stats": {"Strength": 2, "Agility": 5, "Defence": 2, "Luck": 7, "Life": 3, "Wisdom": 4, "Charisma": 7},
+        "equipment_slots": ["helmet", "body_armor", "leg_armor", "boots", "left_ring", "right_ring", "left_bracelet", "right_bracelet", "left_hand", "right_hand", "amulet", "belt", "cloak", "gloves", "back"]
+    },
+    "Orc": {
+        "stats": {"Strength": 7, "Agility": 2, "Defence": 6, "Luck": 2, "Life": 7, "Wisdom": 2, "Charisma": 4},
+        "equipment_slots": ["helmet", "body_armor", "leg_armor", "boots", "left_ring", "right_ring", "left_bracelet", "right_bracelet", "left_hand", "right_hand", "amulet", "belt", "cloak", "gloves", "back"]
+    },
+    "Tiefling": {
+        "stats": {"Strength": 3, "Agility": 3, "Defence": 3, "Luck": 6, "Life": 3, "Wisdom": 6, "Charisma": 6},
+        "equipment_slots": ["helmet", "body_armor", "leg_armor", "boots", "left_ring", "right_ring", "left_bracelet", "right_bracelet", "left_hand", "right_hand", "amulet", "belt", "cloak", "gloves", "back", "horns"]
+    },
+    "Dragonborn": {
+        "stats": {"Strength": 6, "Agility": 3, "Defence": 6, "Luck": 2, "Life": 5, "Wisdom": 4, "Charisma": 4},
+        "equipment_slots": ["helmet", "body_armor", "leg_armor", "boots", "left_ring", "right_ring", "left_bracelet", "right_bracelet", "left_hand", "right_hand", "amulet", "belt", "cloak", "gloves", "back"]
+    },
+    "Lizardfolk": {
+        "stats": {"Strength": 5, "Agility": 4, "Defence": 5, "Luck": 3, "Life": 5, "Wisdom": 3, "Charisma": 2},
+        "equipment_slots": ["helmet", "body_armor", "leg_armor", "boots", "left_ring", "right_ring", "left_bracelet", "right_bracelet", "left_hand", "right_hand", "amulet", "belt", "cloak", "gloves", "back", "tail"]
+    }
+}
